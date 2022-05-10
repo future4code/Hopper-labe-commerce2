c925bae01cf6565d0ae206bfea6024725630274d
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-05-09 23:23:48 -0300

    fiz a função filtrar

--- src/Components/Filtro.js
+++ src/Components/Filtro.js
@@ -18,16 +18,63 @@ const FiltroStyled = styled.div`
 `
 
 class Filtro extends React.Component {
+  state = {
+    valorMaximo: '',
+    valorMinimo: '',
+    buscarProduto: ''
+  }
+
+  onChangeValorMinimo = event => {
+    this.setState({ valorMinimo: event.target.value })
+
+    this.props.filtrarProdutos(
+      this.state.valorMaximo,
+      event.target.value,
+      this.state.buscarProduto
+    )
+  }
+
+  onChangeValorMaximo = event => {
+    this.setState({ valorMaximo: event.target.value })
+
+    this.props.filtrarProdutos(
+      event.target.value,
+      this.state.valorMinimo,
+      this.state.buscarProduto
+    )
+  }
+
+  onChangeBuscarProduto = event => {
+    this.setState({ buscarProduto: event.target.value })
+
+    this.props.filtrarProdutos(
+      this.state.valorMaximo,
+      this.state.valorMinimo,
+      event.target.value
+    )
+  }
+
   render() {
     return (
       <FiltroStyled>
         <h2>Filtros</h2>
         <p>Valor mínimo:</p>
-        <input></input>
+        <input
+          type="number"
+          value={this.state.valorMinimo}
+          onChange={this.onChangeValorMinimo}
+        />
         <p>Valor máximo:</p>
-        <input></input>
+        <input
+          type="number"
+          value={this.state.valorMaximo}
+          onChange={this.onChangeValorMaximo}
+        />
         <p>Busca por nome:</p>
-        <input></input>
+        <input
+          value={this.state.buscarProduto}
+          onChange={this.onChangeBuscarProduto}
+        />
       </FiltroStyled>
     )
   }
