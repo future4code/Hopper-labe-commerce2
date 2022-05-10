4ec21a6b7dc47f9d014d2a3cf35d9929150abbc6
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-05-09 21:19:25 -0300

    Inicio do Projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,175 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import Carrinho from './Components/Carrinho'
+import Filtro from './Components/Filtro'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Estilo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 1rem;
+`
+
+const ProdutosStyled = styled.div`
+  display: flex;
+  flex-direction: ;
+  border: 1px solid;
+`
+const Imagem = styled.img`
+  height: 15rem;
+`
+
+class App extends React.Component {
+  state = {
+    produtos: [
+      {
+        id: 1,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 2,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 3,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 4,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 5,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 6,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 7,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 8,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 9,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      },
+      {
+        id: 10,
+        name: 'Camiseta Astronauta',
+        value: 49.9,
+        imageUrl: (
+          <Imagem
+            src="https://m.media-amazon.com/images/I/71R9NJ0hcML._AC_SX522_.jpg"
+            alt="Imagem do usuario"
+          />
+        )
+      }
+    ]
+  }
+
+  render() {
+    const listaDeProdutos = this.state.produtos.map((produto, index) => {
+      return (
+        <ProdutosStyled>
+          <div>
+            <div>{produto.imageUrl}</div>
+            <p>{produto.name}</p>
+            <p>Valor: R$ {produto.value}</p>
+            <button>Adicionar ao Carrinho</button>
+            {/* key = {index} */}
+          </div>
+        </ProdutosStyled>
+      )
+    })
+
+    return (
+      <Estilo>
+        <div>
+          <Filtro></Filtro>
+        </div>
+
+        <div>
+          <p>Quantidade de produtos: 10</p>
+          <p>
+            Ordenação: <section></section>
+          </p>
+          {listaDeProdutos}
+        </div>
+
+        <div>
+          <Carrinho></Carrinho>
+        </div>
+      </Estilo>
+    )
+  }
 }
 
-export default App;
+export default App
